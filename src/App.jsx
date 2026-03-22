bc24c1b2073ef589257137ebbfee5efd37aa8f50
buchi128 <josephisu4@gmail.com>
2026-03-22 20:23:52 +0100

    Fix casing and import issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/UserDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
-import Loginform from "./pages/LoginForm";
+import Loginform from "./pages/Loginform";
 import AdminLogin from "./pages/AdminLogin"
 import Openaccount from "./pages/Openaccount";
 import RegisterAdmin from "./pages/RegisterAdmin"
@@ -15,26 +15,26 @@ import ImportantLinks from "./components/ImportantLinks";
 import Resources from "./components/Resources";
 import Transactions from "./components/transactions/TransactionList";
 
-let token = localStorage.token;
+const token = localStorage.getItem("token");
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/importantlinks" element={<ImportantLinks />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/transactions" element={<Transactions />} />
-        <Route path="/register" element={<Openaccount />} />
-        <Route path="/admin/seed-admin" element={<RegisterAdmin />} />
-        <Route path="/login" element={<Loginform />} />
-        <Route path="/logout" element={<LogoutButton />} />
-        <Route path="/admin/login" element={<AdminLogin />} /> {/* create this */}
-        <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />
-        <Route path="/admin/dashboard" element={token ? <AdminDashboard /> : <Navigate to="/admin/login" />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />;
+        <Route path="/about" element={<About />} />;
+        <Route path="/importantlinks" element={<ImportantLinks />} />;
+        <Route path="/resources" element={<Resources />} />;
+        <Route path="/transactions" element={<Transactions />} />;
+        <Route path="/register" element={<Openaccount />} />;
+        <Route path="/admin/seed-admin" element={<RegisterAdmin />} />;
+        <Route path="/login" element={<Loginform />} />;
+        <Route path="/logout" element={<LogoutButton />} />;
+        <Route path="/admin/login" element={<AdminLogin />} />; {/* create this */}
+        <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />;
+        <Route path="/admin/dashboard" element={token ? <AdminDashboard /> : <Navigate to="/admin/login" />} />;
+        <Route path="*" element={<PageNotFound />} />;
       </Routes>
       <Footer />
     </>
