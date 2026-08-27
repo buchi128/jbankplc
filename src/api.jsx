2cffda0747777f78ccd5c0636cc818753be10906
buchi128 <josephisu4@gmail.com>
2026-08-27 18:53:00 +0100

    hotfix: point api baseURL to new vercel backend instance

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jbank-backend-lajm.onrender.com"
+  baseURL: "https://vercel.app"
 });
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
--- src/services/createAccount.jsx
+++ src/services/createAccount.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import API from "@/api"
 
 //const API = axios.create({ baseURL: '/api' });
